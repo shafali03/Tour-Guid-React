45fa7a5f65d9e880285cdeceaed63eab04219ab6
shafali03 <shaf03sm@gmail.com>
2019-06-24 19:25:40 +0100

    complete React tour

--- city-tour/src/components/Tour/Tour.js
+++ city-tour/src/components/Tour/Tour.js
@@ -1,8 +1,18 @@
 
 import React, { Component } from 'react'
 import "./tour.scss";
-
 export default class Tour extends Component {
+    state = {
+        showInfo: false
+    };
+    // hide and show info
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        })
+    }
+
+
     render() {
 
         const { id, city, img, name, info } = this.props.tour;
@@ -12,18 +22,22 @@ export default class Tour extends Component {
         return (
             <article className="tour">
                 <div className="img-container">
-                    <img src={img} alt="tour images" />
-                    <span className="close-btn">
+                    <img src={img} alt="City tour" />
+                    <span className="close-btn" onClick={() => removeTour(id)}>
                         <i className="fas fa-window-close" />
                     </span>
                 </div>
                 <div className="tour-info">
                     <h3>{city}</h3>
                     <h4>{name}</h4>
-                    <h5>info <span><i className="fas fa-caret-square-down" />
-                    </span>
+                    <h5>
+                        info{" "}
+                        {/* onclick to show info */}
+                        <span onClick={this.handleInfo}>
+                            <i className="fas fa-caret-square-down" />
+                        </span>
                     </h5>
-                    <p>{info}</p>
+                    {this.state.showInfo && <p>{info}</p>}
                 </div>
             </article>
         )
--- city-tour/src/components/TourList/index.js
+++ city-tour/src/components/TourList/index.js
@@ -8,8 +8,13 @@ export default class TourList extends Component {
         tours: tourData
     };
     removeTour = id => {
+        const { tours } = this.state;
+        const sortedTours = tours.filter(tour => tour.id !== id);
+        this.setState({
+            tours: sortedTours
+        })
+    };
 
-    }
     render() {
         //console.log(this.state.tours)
         const { tours } = this.state;
@@ -18,7 +23,7 @@ export default class TourList extends Component {
             <section className="tourlist">
                 {
                     tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour} ></Tour>))
-    }
+                }
             </section>
         );
     }
